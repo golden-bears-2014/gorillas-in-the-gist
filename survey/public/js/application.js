20f2dd71c32ef3e7b899f2aa9e712b6528457e1c
Jackie Johnston & Katherine Hayden <pair+jackiejohnston.katherineimogene@devbootcamp.com>
2014-03-21 14:30:10 -0700

    WIP, committing to sanitize our git workflow

--- survey/public/js/application.js
+++ survey/public/js/application.js
@@ -1,7 +1,85 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+$(document).ready(function () {
+    $('#new-question').hide()
+    $('#new-choice').hide()
+    // intercept the form submission event using jQuery:
+  $('#new-survey').on('submit', function(e){
+    // prevent the default action for that event from happening:
+    e.preventDefault()
+    // serialize the form's key-value pairs
+    form_content = $(this).serialize()
+    // use jQuery to submit an AJAX post to the form's action:
+    $.ajax({
+      type: this.method,
+      url: this.action,
+      data: form_content
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    }).done(function(server_content){
+      // hide survey form
+      $('#new-survey').hide()
+      // show survey name
+      $('#title').append(server_content)
+      // show question form:
+      $('#new-question').show()
+      console.log('done')
+      console.log('server_content: ' + server_content)
+
+    }).fail(function(){
+      console.log('fail')
+    })
+
+  })
+
+
+  //   // intercept the form submission event using jQuery:
+  // $('#new-question').on('submit', function(e){
+  //   // prevent the default action for that event from happening:
+  //   e.preventDefault()
+  //   // serialize the form's key-value pairs
+  //   form_content = $(this).serialize()
+  //   // use jQuery to submit an AJAX post to the form's action:
+  //   $.ajax({
+  //     type: this.method,
+  //     url: this.action,
+  //     data: form_content
+
+  //   }).done(function(server_content){
+  //     // hide question form
+  //     // show new question
+  //     // show add choice button
+  //     console.log('done')
+  //     console.log('server_content: ' + server_content)
+
+  //   }).fail(function(){
+  //     console.log('fail')
+  //   })
+
+  // })
+
+
+  //   // intercept the form submission event using jQuery:
+  // $('#new-choice').on('submit', function(e){
+  //   // prevent the default action for that event from happening:
+  //   e.preventDefault()
+  //   // serialize the form's key-value pairs
+  //   form_content = $(this).serialize()
+  //   // use jQuery to submit an AJAX post to the form's action:
+  //   $.ajax({
+  //     type: this.method,
+  //     url: this.action,
+  //     data: form_content
+
+  //   }).done(function(server_content){
+  //     // hide question form
+  //     // show new question
+  //     // show add choice button
+  //     console.log('done')
+  //     console.log('server_content: ' + server_content)
+
+  //   }).fail(function(){
+  //     console.log('fail')
+  //   })
+
+  // })
+
+
 });
