fc9d808c252f07f5a945d78b106de71c5aebccaf
Namrata & Melanie Archer <pair+LoveRuby.mejarc@devbootcamp.com>
2014-03-21 18:54:40 -0700

    seeded the db with faker data

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+require 'faker'
+
+
+user = User.new(name: Faker::Name.name, email: Faker::Internet.email)
+user.password = "password"
+user.save!
+
+survey_names = ["Women in tech"]
+
+10.times do
+survey_names.each do |survey|
+   s = Survey.create(name: Faker::Company.catch_phrase)
+  end
+end
